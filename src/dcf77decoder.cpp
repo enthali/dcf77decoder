240010bff8205173918326d9b597745f9e9645de
georg <georg.m.doll@gmail.com>
2021-09-12 00:12:40 +0200

    Significant rework, enabling unit testing.

--- src/dcf77decoder.cpp
+++ src/dcf77decoder.cpp
@@ -12,7 +12,6 @@ the decoder features a counting clock that allows to progress the time in case t
 #undef DCF_DEBUG_BITSTREAM
 #undef DCF_DEBUG_PARITY
 #undef DCF_DEBUG_DECODE
-#define COUNT_CLOCK_DEBUG
 
 /* constants */
 // bit time of a DCF 0 is 100 ms
@@ -21,15 +20,6 @@ the decoder features a counting clock that allows to progress the time in case t
 #define DCF_ONE 256
 // the "missing" 59th second give a minute pules of > 1800 ms
 #define DCF_MIN 1280
-
-// private vairables
-unsigned char signal, buffer;                // checking the input signal and keeping a history
-unsigned char bit;                           // the current bit in the bitstream
-unsigned char dcf77signalPin, dcf77resetPin; // the pins the DCF module is connected to
-unsigned long int timeStamp;                 // to calculate the time passed since the last signal change
-unsigned int deltaTime;                      // to measure the time between signal changes
-// a second timer based on system millis() to advance the seconds and keep the clock running when there's bad signal
-unsigned long int secTimer;
 
 // structs to decode the DCF77 bit stream
 struct dcfStreamStruct
@@ -59,7 +49,16 @@ struct dcfStreamStruct
 // buffer to receive the dcf bitstream
 unsigned long long dcf77BitStream = 0; // 0xa6a6a6a6a6a6a6a6;
 // structure to capture the decoded dcfTime
-tinyTime dcfTime;
+tinyTime dcfInternalTime;
+
+// private vairables
+uint8_t buffer;                              // to store the signal history
+unsigned char dcf77signalPin, dcf77resetPin; // the pins the DCF module is connected to
+unsigned long timeStamp;                     // to calculate the time passed since the last signal change
+unsigned int deltaTime;                      // to measure the time between signal changes
+
+// a second timer based on system millis() to advance the seconds and keep the clock running when there's bad signal
+unsigned long int secTimer;
 
 // private  functions forward declaraions
 extern int parityCheck(struct dcfStreamStruct *pDcfMsg); // parity check
@@ -81,189 +80,155 @@ int dcfSetup(uint8_t signalPin, uint8_t resetPin)
 };
 
 /* advanceClock
-This function is to be called every second, it will simply advance time 
-the function must be called more than 1x per second */
+This function increments the time by one second
+*/
 void advanceCountClock()
 {
-    // the function must be called more than 1x per second
-    //lets check how many millis have passed since last call
-    int delta;
-    delta = millis() - secTimer;
-    if (delta >= 1000)
+    dcfInternalTime.sec += 1;
+    if (dcfInternalTime.sec >= 60)
     {
-        // more than 1000
-        // advance the secTimer
-        secTimer+=delta;
-
-        dcfTime.sec += 1;
-        if (dcfTime.sec >= 60)
-        {
-            dcfTime.sec = 0;
-            dcfTime.min += 1;
-            // if we got to count minutes, something's wrong with the DCF signal. lets set the time status to internal
-            dcfTime.status = STATUS_COUNT;
-            if (dcfTime.min >= 60)
-            {
-                dcfTime.min = 0;
-                dcfTime.hour += 1;
-
-                if (dcfTime.hour >= 24)
-                {
-                    dcfTime.hour = 0;
-                    dcfTime.weekDay += 1;
-                    if (dcfTime.weekDay >= 8)
-                    {
-                        dcfTime.weekDay = 0;
-                    }
-                    // here's where we need to implement day and month advancement ...
-                }
-            }
-        }
-#ifdef COUNT_CLOCK_DEBUG
-        Serial.print(dcfTime.weekDay);
-        Serial.print(" / ");
-        Serial.print(dcfTime.hour);
-        Serial.print(":");
-        Serial.print(dcfTime.min);
-        Serial.print(":");
-        Serial.print(dcfTime.sec);
-        Serial.print("   STATUS: ");
-        Serial.print(dcfTime.status);
-        Serial.println();
-#endif
+        dcfInternalTime.sec = 0;
+        dcfInternalTime.min += 1;
     }
+
+    if (dcfInternalTime.min >= 60)
+    {
+        dcfInternalTime.min = 0;
+        dcfInternalTime.hour += 1;
+    }
+    if (dcfInternalTime.hour >= 24)
+    {
+        dcfInternalTime.hour = 0;
+        dcfInternalTime.weekDay += 1;
+    }
+    if (dcfInternalTime.weekDay >= 8)
+    {
+        dcfInternalTime.weekDay = 0;
+    }
+    // here's where we need to implement day and month advancement ...
+}
+
+/*
+wird diese Funktion aufgerufen gibt sie die Anzahl der vergangenen Sekunden seit letztem Aufruf zurück
+*/
+// internal variables
+int delta = 0;
+
+/* 
+check how much time passed since the last call. if it's more than a second, go ahead and trigger the second
+*/
+int checkSecond(unsigned long time)
+{
+    int secCount = 0;
+    // how much time has passed?
+    delta = time - secTimer;
+
+    // if  1000 or more msec have passed, let's find out how much time has passed
+    for (secCount = 0; delta >= 1000; secCount++)
+    {
+        delta -= 1000;       // reduce the delta
+        secTimer += 1000;    // increase the secTime reference counter
+        advanceCountClock(); // count the second
+    }
+    return (secCount);
 }
 
 // function to set the current count clock time
 void setTime(struct tinyTime tTime)
 {
-    dcfTime = tTime;
+    dcfInternalTime = tTime;
 }
 
 // function to get the current clock count time
 tinyTime getTime()
 {
-    return (dcfTime);
+    return (dcfInternalTime);
 }
 
-/* this function hast to be called periodically to sample the input pin 
-the shortes signal time the dcf 77 decoder provides is a 100ms signal, to safely detect this signal
-the sample function has to be called at least every 50ms */
-
-int dcfCheckSignal()
+/* if signalDecode checks if the signal level changed and returns the signal code
+0 - a bit value 0 was detected
+1 - a bit value 1 was detected
+2 - the minute pulse was detected
+3 - bit pause detected
+4 - no change detected
+signalDecode expects the system time in millis and should be called frequently
+*/
+uint8_t signalDecode(unsigned long sysTime)
 {
-    advanceCountClock(); // lets see if we should advance the time
+    uint8_t signal = 0;
 
-    int retval = 0;
     signal = digitalRead(dcf77signalPin); // get the current timestamp
 
     // Check if the signal has changed since the last call.
     if (buffer != signal)
     // Yes, the signal has changed
     {
-        // get the current system time, and calculate the time that has elapsed since the last call.
-        deltaTime = millis() - timeStamp;
-
-#ifdef DCF_DEBUG_TIMESTAMP
-        // Debug output of the signal timestamp
-        Serial.print("DCF77 signal : ");
-        Serial.print(signal);
-        Serial.print(" - delta time : ");
-        Serial.print(deltaTime);
-        Serial.println(" ");
-#endif
-
-#ifdef DCF_DEBUG_RAW_DATA
-        // Debug output of the raw DCF77 bit signal
-        if (deltaTime < DCF_ZERO)
-        {
-            Serial.println("DCF Value : 0");
-        }
-        else if (deltaTime < DCF_ONE)
-        {
-            Serial.println("DCF Value : 1");
-        }
-        else if (deltaTime > DCF_MIN)
-        {
-            Serial.println("DCF Value : Minute Pulse");
-        }
-#endif
-        // the DCF77decode code starts here:
-        if (deltaTime < DCF_ZERO)
-        {
-            // shift the bit stream by 1 to the left but don'T add anything
-            dcf77BitStream >>= 1;
-        }
-        else if (deltaTime < DCF_ONE)
-        {
-            // shift the bit stream by 1 to the left
-            dcf77BitStream >>= 1;
-            dcf77BitStream |= 0x8000000000000000;
-        }
-        else if (deltaTime > DCF_MIN)
-        {
-            // move the bit stream entirely to the right
-            dcf77BitStream >>= 5;
-#ifdef DCF_DEBUG_BITSTREAM
-            printBitstream();
-#endif
-            // check if a valid, consistent telegram was received
-            if (parityCheck((dcfStreamStruct *)&dcf77BitStream))
-            {
-                // ok go on
-                if (decodeTime((dcfStreamStruct *)&dcf77BitStream))
-                {
-                    // ok we got a valid signal
-                    retval = 1; // everything's fine
-                }
-                else
-                {
-                    retval = 0; // the telegram wasn't validated
-                }
-            }
-            else
-            {
-                // no valid telegram we
-            }
-        }
-
-        // we are done for this time arround clean up
-
         // save the signal state for the next call in buffer
         buffer = signal;
-        // calculate the current timestamp for the next callF
-        timeStamp += deltaTime;
-    }
-    return (retval);
-};
 
-#ifdef DCF_DEBUG_BITSTREAM
-void printBitstream()
-{
-    Serial.println();
-    Serial.print("          : ");
-    for (int i = 0; i < 8; i++)
-    {
-        Serial.print("...-...|");
-    }
-    Serial.println();
-    Serial.print("Telegram  : ");
-    unsigned long long mask = 0x8000000000000000;
-    do
-    {
-        if (dcf77BitStream & mask)
-        {
-            Serial.print("1");
-        }
+        // use the current system time, and calculate the time that has elapsed since the last call.
+        deltaTime = sysTime - timeStamp;
+
+        if (deltaTime < DCF_ZERO)
+            return (0);
+        else if (deltaTime < DCF_ONE)
+            return (1);
+        else if (deltaTime > DCF_MIN)
+            return (2);
         else
-        {
-            Serial.print("0");
-        }
-        mask >>= 1;
-    } while (mask > 0);
-    Serial.println();
+            return (3);
+    }
+    return (4);
 }
-#endif
+
+/* this function hast to be called periodically to sample the input pin 
+the shortes signal time the dcf 77 decoder provides is a 100ms signal, to safely detect this signal
+the sample function has to be called at least every 50ms */
+int dcfCheckSignal()
+{
+    timeStamp = millis(); // get the system time for this calculation
+
+    checkSecond(timeStamp); // lets see if we should advance the time
+
+    // calculate the current timestamp for the next callF
+    timeStamp += deltaTime;
+
+    switch (signalDecode(timeStamp))
+    {
+    case 0: // roll a 0 into the bitstream
+        dcf77BitStream >>= 1;
+        break;
+    case 1: // roll a 1 into the bitstream
+        dcf77BitStream >>= 1;
+        dcf77BitStream |= 0x8000000000000000;
+        break;
+    case 2: // move the bit stream entirely to the right
+        dcf77BitStream >>= 5;
+
+        // check if a valid, consistent telegram was received
+        if (parityCheck((dcfStreamStruct *)&dcf77BitStream))
+        {
+            // ok go on
+            if (decodeTime((dcfStreamStruct *)&dcf77BitStream))
+            {
+                // ok we got a valid signal
+                return (1);
+            }
+        }
+        // no valid telegram we
+        return (0);
+        break;
+    case 3: // do nothing
+        break;
+    case 4: // do nothing
+        break;
+    default:
+        break;
+    }
+
+    // no valid telegram we
+    return (0);
+}
 
 /*
 * This function checks the parity bits in the DCF77 telegram.
@@ -272,28 +237,12 @@ void printBitstream()
 */
 int parityCheck(struct dcfStreamStruct *pDcfMsg)
 {
-
-#ifdef DCF_DEBUG_PARITY
-    Serial.print("Parity Check Minutes : ");
-    Serial.println((int)pDcfMsg->parMin);
-    Serial.print("Parity Check Hours : ");
-    Serial.println((int)pDcfMsg->parHour);
-    Serial.print("Parity Check Date : ");
-    Serial.println((int)pDcfMsg->parDate);
-#endif
-
     // check the minute parity bit
     if (pDcfMsg->parMin == (parity(pDcfMsg->minOnes) ^ parity(pDcfMsg->minTens)))
     {
-#ifdef DCF_DEBUG_PARITY
-        Serial.println(" Minute Parity OK ");
-#endif
         // check the parity bit of the hours
         if (pDcfMsg->parHour == (parity(pDcfMsg->hourOnes) ^ parity(pDcfMsg->hourTens)))
         {
-#ifdef DCF_DEBUG_PARITY
-            Serial.println(" Hour Parity OK");
-#endif
             // check the parity bit of the date
             if (pDcfMsg->parDate == ((((((
                                              parity(pDcfMsg->dayOnes) ^
@@ -304,51 +253,22 @@ int parityCheck(struct dcfStreamStruct *pDcfMsg)
                                       parity(pDcfMsg->yearOnes)) ^
                                      parity(pDcfMsg->yearTens)))
             {
-#ifdef DCF_DEBUG_PARITY
-                Serial.println(" Date Parity OK");
-#endif
                 // everything is seems fine, so let's decode the time
                 return (1); // everything's fine
             }
-            else
-            {
-#ifdef DCF_DEBUG_PARITY
-                Serial.println(" Date Parity ERROR");
-#endif
-            }
-        }
-        else
-        {
-#ifdef DCF_DEBUG_PARITY
-            Serial.println(" Hour Parity ERROR");
-#endif
         }
     }
-    else
-    {
-#ifdef DCF_DEBUG_PARITY
-        Serial.println(" Minute Parity ERROR ");
-#endif
-    }
-    return (0); // we don't have a valid time (yet)
+    // telegram bad
+    dcfInternalTime.status = STATUS_DCF_BAD;
+    return (0); // we don't have a valid time
 }
 
 // calculate the parity of the lower 4 bit of an unsigned 8 bit word;
 uint8_t parity(uint8_t value)
 {
-#ifdef DCF_DEBUG_PARITY
-    Serial.println(value, BIN);
-#endif
     value ^= value >> 2; // xor the lower 4 bit result into the lower 2
-#ifdef DCF_DEBUG_PARITY
-    value &= 3; // to ease debugging kill all above the least significant two bits
-    Serial.println(value, BIN);
-#endif
     value ^= value >> 1; // yor the lower 2 bits result into lowest bit
     value &= 1;          // parity bit is now the least significant bit, so erase any remaining the rest
-#ifdef DCF_DEBUG_PARITY
-    Serial.println(value, BIN);
-#endif
     return (value);
 }
 
@@ -358,37 +278,36 @@ uint8_t parity(uint8_t value)
 */
 int decodeTime(struct dcfStreamStruct *pDcfMsg)
 {
-#ifdef DCF_DEBUG_DECODE
-    Serial.print((int)pDcfMsg->hourTens);
-    Serial.print((int)pDcfMsg->hourOnes);
-    Serial.print(":");
-    Serial.print((int)pDcfMsg->minTens);
-    Serial.print((int)pDcfMsg->minOnes);
-    Serial.print(" weekday : ");
-    Serial.println((int)pDcfMsg->weekday);
-#endif
-    // so fare so good
-    // simple check if the last minute received +1 is the same as this time arround
-    if ((pDcfMsg->minOnes + pDcfMsg->minTens * 10) == dcfTime.min + 1)
+    // simple check. check if the actual time is the same as the DCF signal.
+    if ((pDcfMsg->minOnes + pDcfMsg->minTens * 10) == dcfInternalTime.min + 1)
     {
-        // ok looks like we received two telegrams in seqence, therefore set the time status to good
-        dcfTime.status = STATUS_DCF;
+        //telegram plausible, set the time status to good
+        dcfInternalTime.status = STATUS_DCF_GOOD;
     }
     else
     {
-        // this is apparently the first telegram we received in a sequence
-        dcfTime.status = STATUS_FIRST;
+        // was the last telegram consistent?
+        if (dcfInternalTime.status = STATUS_DCF_SINGLE)
+        {
+            dcfInternalTime.status = STATUS_DCF_GOOD;
+        }
+        else
+        {
+            // the telegram is not in sequence, set the status to SINGLE
+            dcfInternalTime.status = STATUS_DCF_SINGLE;
+        }
     }
     secTimer = millis(); // reset the second counter
-    dcfTime.sec=0;
-    dcfTime.min = pDcfMsg->minOnes + pDcfMsg->minTens * 10;
-    dcfTime.hour = pDcfMsg->hourOnes + pDcfMsg->hourTens * 10;
-    dcfTime.weekDay = pDcfMsg->weekday;
+    dcfInternalTime.sec = 0;
+    dcfInternalTime.min = pDcfMsg->minOnes + pDcfMsg->minTens * 10;
+    dcfInternalTime.hour = pDcfMsg->hourOnes + pDcfMsg->hourTens * 10;
+    dcfInternalTime.weekDay = pDcfMsg->weekday;
+
     // set the system time
-    if (dcfTime.status == STATUS_DCF)
+    if (dcfInternalTime.status == STATUS_DCF_GOOD)
     {
-        setTime(dcfTime);
-        return (1); // we have a valid time
+        setTime(dcfInternalTime);
+        return (1); // we have a validated time
     }
-    return (0); // no valid time (yet
+    return (0); // no valid time
 }
